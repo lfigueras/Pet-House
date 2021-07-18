842a6350069e976daf678375453557d292cd931b
Lovely Joy <lovelyfigueras@gmail.com>
2021-07-18 23:15:10 +0800

    nested form error

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Website
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
